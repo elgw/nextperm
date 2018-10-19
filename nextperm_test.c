a5ccbc0769d658a2a5aadcbabb1bfd4a1e83093a
elgw <erik.wernersson@gmail.com>
2018-10-19 07:47:37 +0200

    Checked return value of nextperm_init

--- nextperm_test.c
+++ nextperm_test.c
@@ -6,19 +6,24 @@
 int main(int argc, char ** argv)
 {
   int N = 4;
-int pnum = 1;
+  int pnum = 1;
   if(argc == 2)
     N = atoi(argv[1]);
 
   if(N<1)
     return 0;
 
-  perm_t * P = perm_init(N);  
+  perm_t * P = perm_init(N);
+  if(P==NULL)
+  {
+    printf("Allocation failed\n");
+    return 1;
+  }
 
   printf("#%03d ", pnum++); perm_show(P);
   while(perm_next(P) == 0)
   {
-  printf("#%03d ", pnum++);    perm_show(P);
+    printf("#%03d ", pnum++);    perm_show(P);
   }
   perm_free(P);
 
